77df5d03f13a2ffe0ff08e3071d7ca25943e1d89
pydamnaidudogga <pydamnaidudogga@gmail.com>
2023-08-21 09:24:43 +0530

    fixing google authentication problem

--- src/components/form/page.jsx
+++ src/components/form/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../../styles/form.css";
 import { signIn } from 'next-auth/react';
-import {ToastContainer} from 'react-toastify';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
  
 
